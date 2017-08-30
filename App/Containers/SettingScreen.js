de8c5770c4d5d2e0981521fdc699a1e56fdf65c1
ahsan <ahsan_008_2010@yahoo.com>
2017-08-30 18:33:06 +0500

    Toggle button in progress

--- App/Containers/SettingScreen.js
+++ App/Containers/SettingScreen.js
@@ -1,11 +1,13 @@
 import React, {Component} from "react";
-import {View, Text, Switch} from 'react-native'
+import {View, Text, Switch, PushNotificationIOS, AppState, Alert, Linking} from 'react-native'
 import LinearGradient from 'react-native-linear-gradient'
-import {connect} from "react-redux";
-import {Button, Icon} from 'react-native-elements';
+import {connect} from "react-redux"
+import {Button, Icon} from 'react-native-elements'
 import moment from 'moment'
 import {responsiveWidth} from 'react-native-responsive-dimensions'
 import DateTimePicker from 'react-native-modal-datetime-picker'
+const Permissions = require('react-native-permissions')
+// var PushNotification = require('react-native-push-notification')
 
 // Styles
 import {Colors} from '../Themes'
@@ -32,13 +34,88 @@ class SettingScreen extends Component {
         super(props)
         this.state = {
             dailyNotification: false,
-            time: moment().format("h:mm"),
+            pushNotification: false,
+            time: moment().format("h:mm A"),
             actualTime: moment.utc(),
             isDateTimePickerVisible: false,
         }
+        this._handleAppStateChange = this._handleAppStateChange.bind(this)
+        this.checkNotificationPermissions = this.checkNotificationPermissions.bind(this)
     }
 
     componentDidMount() {
+        AppState.addEventListener('change', this._handleAppStateChange)
+    }
+
+    componentWillUnmount() {
+        AppState.removeEventListener('change', this._handleAppStateChange)
+        // PushNotification.localNotification({
+        //     title: "My Notification Title",
+        //     message: "My Notification Message",
+        // })
+    }
+
+    checkNotificationPermissions() {
+        //response is one of: 'authorized', 'denied', 'restricted', or 'undetermined'
+        Permissions.check('notification')
+            .then(response => {
+                if (response == 'undetermined') {
+                    Permissions.request('notification')
+                        .then(response => {
+                            if (response == 'authorized') {
+                                this.setState({dailyNotification: true, pushNotification: true})  //TODO : IN SAGAS
+                            }
+                        })
+                } else if (response == 'denied') {
+                    Alert.alert(
+                        'Enable Push Notification',
+                        'Press "Ok" and select "Allow Notifications" ',
+                        [
+                            {
+                                text: 'OK', onPress: () => {
+                                const url = 'app-settings:'
+
+                                Linking.canOpenURL(url).then(supported => {
+                                    if (!supported) {
+                                        console.log('Can\'t handle url: ' + url);
+                                    } else {
+                                        return Linking.openURL(url);
+                                    }
+                                }).catch(err => console.error('An error occurred', err));
+                            }
+                            },
+                        ],
+                        {cancelable: false}
+                    )
+                } else if (response == 'authorized') {
+                    this.setState({dailyNotification: true, pushNotification: true})//TODO : IN SAGAS
+                }
+            })
+    }
+
+    _handleAppStateChange(appState) {
+        if (appState == 'active') {
+            this.checkNotificationPermissions()
+        }
+    }
+
+    _onRegistered(deviceToken) {
+        console.log('Device Token: ', deviceToken);
+        // Update device push token against current device
+        // const {dispatch} = this.props
+        // dispatch(Actions.updatePushToken(deviceToken))
+    }
+
+    onNotification(notification) {
+        console.log('OnNotify', notification)
+    }
+
+    handleSwitch(value) {
+        if (value) {
+            this.checkNotificationPermissions()
+        }else {
+            this.setState({dailyNotification: false, pushNotification: false})  //TODO : IN SAGAS
+        }
     }
 
     handlePressTime() {
@@ -48,11 +125,10 @@ class SettingScreen extends Component {
     _handleTimePicked = (time) => {
         this.setState({
             isDateTimePickerVisible: false,
-            time: moment(time).format("h:mm"),
+            time: moment(time).format("h:mm A"),
             actualTime: moment(time).utc()
         })
     }
-
 
     render() {
         return (
@@ -68,14 +144,14 @@ class SettingScreen extends Component {
                         <Text style={{color: Colors.transparentGrey, fontSize: 25}}>Daily Notifications</Text>
                         <Switch
                             style={{}}
-                            onValueChange={(value) => this.setState({dailyNotification: !this.state.dailyNotification})}
+                            onValueChange={this.handleSwitch.bind(this)}
                             value={this.state.dailyNotification}
                             disabled={false}/>
                     </View>
 
                     <View style={{alignSelf: 'center', flexDirection: 'row', justifyContent: 'space-between'}}>
                         <Text style={{color: Colors.snow, fontSize: 28}}
-                        onPress={this.handlePressTime.bind(this)}>{this.state.time}</Text>
+                              onPress={this.handlePressTime.bind(this)}>{this.state.time}</Text>
                     </View>
 
                     <View style={{alignSelf: 'center', flexDirection: 'row', justifyContent: 'space-between'}}>
@@ -91,7 +167,9 @@ class SettingScreen extends Component {
                     <DateTimePicker
                         isVisible={this.state.isDateTimePickerVisible}
                         onConfirm={this._handleTimePicked}
-                        onCancel={() => {this.setState({isDateTimePickerVisible: false})}}
+                        onCancel={() => {
+                            this.setState({isDateTimePickerVisible: false})
+                        }}
                         mode='time'
                         titleIOS='Select a time'
                     />
