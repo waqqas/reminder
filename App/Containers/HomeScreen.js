d00ee29ceb5b9e3761b21248a23da47ad2a133ef
ahsan <ahsan_008_2010@yahoo.com>
2017-08-30 14:06:01 +0500

    increment and decrement of dates done ,

--- App/Containers/HomeScreen.js
+++ App/Containers/HomeScreen.js
@@ -1,10 +1,11 @@
 import React, {Component} from "react";
-// import {Button} from 'react-native'
-import {Button} from 'react-native-elements';
+import {Button, Icon} from 'react-native-elements';
+import moment from 'moment'
 
 import {connect} from "react-redux";
 import {View, Text} from 'react-native'
 import LinearGradient from 'react-native-linear-gradient'
+import { responsiveWidth } from 'react-native-responsive-dimensions'
 
 // Styles
 import {Colors} from '../Themes'
@@ -24,10 +25,69 @@ class HomeScreen extends Component {
         })
     }
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            date: moment.utc().format("MMMM D"),
+            actualDate: moment.utc()
+        }
+    }
+
+    handlePressArrow(arrowDirection) {
+        if(arrowDirection == 'RIGHT') {
+            let newDate = this.state.actualDate.add(1, 'days')
+            this.setState({
+                date: newDate.format("MMMM D"),
+                actualDate: newDate
+            })
+        }
+        else if(arrowDirection == 'LEFT') {
+            let newDate = this.state.actualDate.subtract(1, 'days')
+            this.setState({
+                date: newDate.format("MMMM D"),
+                actualDate: newDate
+            })
+        }
+    }
+
     render() {
         return (
             <View style={styles.mainContainer}>
-                <LinearGradient colors={[Colors.gradient1, Colors.gradient2]} style={styles.backgroundImage}>
+                <LinearGradient colors={[Colors.gradient1, Colors.gradient2]} style={[styles.backgroundImage, {justifyContent: 'center', alignItems: 'center'}]}>
+
+                    <View style={{alignSelf: 'center', flexDirection:'row', justifyContent: 'space-between', width: responsiveWidth(70)}}>
+                        <Icon
+                            size={45}
+                            name='caret-left'
+                            type='font-awesome'
+                            color={Colors.transparentGrey}
+                            underlayColor={Colors.transparent}
+                            onPress={this.handlePressArrow.bind(this, 'LEFT')}/>
+                        <Text style={{color: Colors.transparentGrey, fontSize: 35}}>{this.state.date}</Text>
+                        <Icon
+                            size={45}
+                            name='caret-right'
+                            type='font-awesome'
+                            color={Colors.transparentGrey}
+                            underlayColor={Colors.transparent}
+                            onPress={this.handlePressArrow.bind(this, 'RIGHT')}/>
+                    </View>
+
+                    <View style={{alignSelf: 'center', flexDirection:'row', justifyContent: 'space-between'}}>
+                        <Text style={{color: Colors.snow, fontSize: 18}}>Abcdefghij</Text>
+                    </View>
+
+                    <View style={{alignSelf: 'center', flexDirection:'row', justifyContent: 'space-between'}}>
+                        <Icon
+                            size={45}
+                            name='heart'
+                            type='font-awesome'
+                            color={Colors.transparentGrey}
+                            underlayColor={Colors.transparent}
+                            onPress={() => console.log('hello')}/>
+                        <Text style={{color: Colors.transparentGrey, fontSize: 35}}>LOGO IMAGE</Text>
+                    </View>
+
                 </LinearGradient>
             </View>
         )
